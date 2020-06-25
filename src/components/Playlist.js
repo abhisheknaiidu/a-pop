05e70558d50ebf73925797cbc4e270dc84196adc
abhisheknaiidu <geekyabhi12@gmail.com>
2020-06-25 21:05:15 +0530

    Sample play list songs

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Typography } from '@material-ui/core'
 
 function Playlist() {
-    return <div>
-        Playlist
+
+    const song = {
+        title: 'Abhishek',
+        artist: 'Naidu',
+        thumbnail: 'https://avatars0.githubusercontent.com/u/55599878?s=460&u=ed7ab421e2b7b0fdc9fd0ddeae2f73feeb72eede&v=4'
+    }
+
+    return (
+    <div style={{ margin: '10px 0' }}>
+        <Typography color="textSecondary" variant="button">
+            PLAYLIST (5)
+        </Typography>
+        {Array.from({ length: 5 }, () => song).map((song, i) => (
+            <PlaylistSong key={i} song={song} />
+        ))}
     </div>
+    )
+}
+
+function PlaylistSong({ song }) {
+    return (
+        <div>
+            Playlist Song
+        </div>
+    )
 }
 
 export default Playlist;
