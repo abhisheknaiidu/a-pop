76646a27d4cf7abab9e2faefeb12c3e31bb2c908
abhisheknaiidu <geekyabhi12@gmail.com>
2020-06-25 18:54:45 +0530

    Added loading spinner

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,6 +1,23 @@
 import React from 'react'
+import { CircularProgress } from '@material-ui/core';
 
 function SongList() {
+    
+    let loading = true;
+
+    if(loading) {
+        return (
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                marginTop: 50
+            }}>
+                <CircularProgress />
+            </div>
+        )
+    }
+    
     return <div>
         SongList
     </div>
