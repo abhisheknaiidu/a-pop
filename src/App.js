87548ba16e036494e23ac0445bf7cca021e80b05
abhisheknaiidu <geekyabhi12@gmail.com>
2020-06-25 14:48:15 +0530

    Header built

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import Header from './components/Header';
 import AddSong from './components/AddSong';
 import SongList from './components/SongList';
 import SongPlayer from './components/SongPlayer';
+import { Grid } from '@material-ui/core';
 
 function App() {
   return (
     <>
     <Header />
-    <AddSong />
-    <SongList />
-    <SongPlayer />
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={7}>
+      <AddSong />
+      <SongList />
+      </Grid>
+      <Grid item xs={12} md={5}>
+      <SongPlayer />
+      </Grid>
+    </Grid>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
+import { HeadsetTwoTone } from '@material-ui/icons';
+
+const useStyles = makeStyles({
+    heading: {
+        marginLeft: '10px'
+    }
+})
 
 function Header() {
-    return <div>
-        Header
-    </div>
+    //Styles can be used as hooks.
+    const classes = useStyles() 
+
+    return (
+        <AppBar position="fixed">
+            <Toolbar>
+                <HeadsetTwoTone />
+                <Typography className={classes.heading} variant="h6" component="h1">
+                    Zepta Music Sharing
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
 }
 
 export default Header;
