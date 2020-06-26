763d53e66e740998ae780f4f1aeab4d09761622b
abhisheknaiidu <geekyabhi12@gmail.com>
2020-06-26 17:09:18 +0530

    Modified in order to use subscriptions 😭

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,7 +1,23 @@
-import ApolloClient from 'apollo-boost'
+// New Changes are done in order to use the subscription tool!
+import ApolloClient from 'apollo-client'
+import { WebSocketLink } from 'apollo-link-ws'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-const client = new ApolloClient( {
-    uri: 'https://zepta-music.herokuapp.com/v1/graphql'
+// import ApolloClient from 'apollo-boost'
+
+const client = new ApolloClient({
+    link: new WebSocketLink({
+        uri: 'wss://zepta-music.herokuapp.com/v1/graphql',
+        options: {
+            //Connects whenever it's possible
+            reconnect: true
+        }
+    }),
+    cache: new InMemoryCache()
 })
+
+// const client = new ApolloClient( {
+//     uri: 'https://zepta-music.herokuapp.com/v1/graphql'
+// })
 
 export default client;
