41181043acd8971e262c03d6708d0f1b087fdcf4
abhisheknaiidu <geekyabhi12@gmail.com>
2020-06-26 11:58:24 +0530

    Client setup

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { MuiThemeProvider, CssBaseline } from '@material-ui/core'
 import theme from './theme'
+import { ApolloProvider } from '@apollo/react-hooks'
+import client from './graphql/client';
 
 ReactDOM.render(
+    <ApolloProvider client={client}>
     <MuiThemeProvider theme={theme}>
         <CssBaseline />
         <App />
-    </MuiThemeProvider>,
+    </MuiThemeProvider>
+    </ApolloProvider>,
  document.getElementById('root')
 );
 
