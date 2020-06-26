bf521b1b9daf61cb169bb644dd20443e8c217af9
abhisheknaiidu <geekyabhi12@gmail.com>
2020-06-26 15:13:08 +0530

    Mutations addded 🎖

---
+++ src/graphql/mutations.js
@@ -0,0 +1,10 @@
+import { gql } from 'apollo-boost'
+
+export const ADD_SONG = gql `
+mutation addSong($title: String!, $artist: String!, $thumbnail: String!, $duration: Float!, $url: String!) {
+    insert_songs(objects: {title: $title, artist: $artist, thumbnail: $thumbnail, duration: $duration, url: $url}) {
+      affected_rows
+    }
+  }
+`
+
