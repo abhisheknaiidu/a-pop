b668d96911347c37b96402b7e5802f0e6a1fc410
abhisheknaiidu <geekyabhi12@gmail.com>
2020-06-27 13:03:11 +0530

    Added apollo tools to client 🛬

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -17,9 +17,45 @@ const client = new ApolloClient({
     // This has been done by hasura previously, creating these typeDefs for us 
     // , creating these shema's for us  but now we need to tell apollo
     // what we quering for, the data itself or any mutations
+
     typeDefs: gql`
+      type Song {
+        id: uuid!,
+        thumbnail: String!,
+        artist: String!,
+        title: String!,
+        duration: Float!,
+        url: String!
+    }
+
+    input SongInput {
+        id: uuid!,
+        thumbnail: String!,
+        artist: String!,
+        title: String!,
+        duration: Float!,
+        url: String!
+    }
+
+    type Query {
+        playlist: [Song]!,
+    }
+    
+    type Mutation {
+        addOrRemoveFromPlaylist(input: SongInput!): [Song]! 
+    }
     `
+    // returns array of type song
+    // So we defined to apollo, what exactly our song data woulb be consists of,
+    // how to query the data and how to make mutations to it
 })
+
+const data = {
+    playlist: []
+}
+
+// the way through which we give the created client the data
+client.writeData({ data })
 
 // const client = new ApolloClient( {
 //     uri: 'https://zepta-music.herokuapp.com/v1/graphql'
