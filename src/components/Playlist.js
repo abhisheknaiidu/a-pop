baf20f52bcb21f14a211fd7bda50220df3bbafea
abhisheknaiidu <geekyabhi12@gmail.com>
2020-06-27 18:58:20 +0530

    Playlist frontend done 😭

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -30,18 +30,18 @@ function Playlist( {playlist} ) {
     console.log( {playlist} )
     const greaterThanMd = useMediaQuery( theme => theme.breakpoints.up('md'))
 
-    const song = {
-        title: 'Abhishek',
-        artist: 'Naidu',
-        thumbnail: 'https://avatars0.githubusercontent.com/u/55599878?s=460&u=ed7ab421e2b7b0fdc9fd0ddeae2f73feeb72eede&v=4'
-    }
+    // const song = {
+    //     title: 'Abhishek',
+    //     artist: 'Naidu',
+    //     thumbnail: 'https://avatars0.githubusercontent.com/u/55599878?s=460&u=ed7ab421e2b7b0fdc9fd0ddeae2f73feeb72eede&v=4'
+    // }
 
     return greaterThanMd && (
     <div style={{ margin: '10px 0' }}>
         <Typography color="textSecondary" variant="button">
-            PLAYLIST (5)
+            PLAYLIST (playlist.length)
         </Typography>
-        {Array.from({ length: 5 }, () => song).map((song, i) => (
+        {playlist.map((song, i) => (
             <PlaylistSong key={i} song={song} />
         ))}
     </div>
