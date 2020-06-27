d94996ecba8e335b497ca5301d9649b2693aedfc
abhisheknaiidu <geekyabhi12@gmail.com>
2020-06-27 20:00:09 +0530

    Formatted played seconds 😭

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -38,6 +38,7 @@ const useStyles = makeStyles( theme => ({
 function SongPlayer() {
     const classes = useStyles()
 
+    const [playedSeconds, setPlayedSeconds] = React.useState(0)
     const reactPlayerRef = React.useRef()
     const [played, setPlayed] = React.useState(0)
     const [seeking, setSeeking] = React.useState(false)
@@ -60,6 +61,10 @@ function SongPlayer() {
     function handleSeekMouseUp() {
         setSeeking(false)
         reactPlayerRef.current.seekTo(played)
+    }
+
+    function formatDuration(seconds) {
+        return new Date(seconds*1000).toISOString().substr(11,8)
     }
 
     return (
@@ -85,7 +90,7 @@ function SongPlayer() {
                     <SkipNext/>
                 </IconButton>
                 <Typography variant="subtitle1" component="p" color="textSecondary">
-                        00:01:30
+                        {formatDuration(playedSeconds)}
                 </Typography>
                 </div>
                 <Slider
@@ -104,6 +109,7 @@ function SongPlayer() {
             onProgress={ ({ played, playedSeconds }) => {
                 if(!seeking) { 
                     setPlayed(played)
+                    setPlayedSeconds(playedSeconds)
                 }
             }}
             url={state.song.url} playing={state.isPlaying} hidden />
