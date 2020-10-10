7bf94d0af0c79723f496e79d41489ef66917bf5e
abhisheknaiidu <abhisheknaiidu@users.noreply.github.com>
2020-10-10 13:26:02 +0000

    Prettified Code!

--- public/worker.js
+++ public/worker.js
@@ -1,33 +1,33 @@
-let cacheName = 'A-POP';
-let contentToCache = [
-  '/',
-];
+let cacheName = "A-POP";
+let contentToCache = ["/"];
 
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
   e.waitUntil(
-      caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching assets');
-          cache.addAll(contentToCache);
-      })
+    caches.open(cacheName).then((cache) => {
+      console.log("[Service Worker] Caching assets");
+      cache.addAll(contentToCache);
+    })
   );
 });
 
-self.addEventListener('activate', (e) => {
-e.waitUntil(
-  caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-        if(key !== cacheName) {
-          return caches.delete(key);
-        }
-    }));
-  })
-);
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(res=>{
+    caches.match(e.request).then((res) => {
       return res || fetch(e.request);
     })
   );
